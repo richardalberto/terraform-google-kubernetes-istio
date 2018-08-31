3d3a2333995bb8c1e1d81a900a8d58f348971c40
Richard Alberto <r@richardalberto.com>
2018-08-31 12:53:34 -0400

    Corrected outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,23 @@
-output "cluster_name" {
-  value = "${google_container_cluster.gke_cluster.name}"
+output "url" {
+  value = "https://${google_container_cluster.gke_cluster.endpoint}"
+}
+
+output "admin_username" {
+  value = "${var.master_username}"
+}
+
+output "admin_password" {
+  value = "${var.master_password}"
+}
+
+output "client_certificate" {
+  value = "${base64decode(google_container_cluster.gke_cluster.master_auth.0.client_certificate)}"
+}
+
+output "client_key" {
+  value = "${base64decode(google_container_cluster.gke_cluster.master_auth.0.client_key)}"
+}
+
+output "cluster_ca_certificate" {
+  value = "${base64decode(google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)}"
 }
